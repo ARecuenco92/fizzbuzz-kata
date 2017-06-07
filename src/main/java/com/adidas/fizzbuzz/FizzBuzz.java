fe80f4c972e2ee3f10e0e44d3edb3ef67812e37f
recuealv <Alvaro.Recuenco@adidas-group.com>
2017-06-07 14:19:58 +0200

    FizzBuzz class & test

---
+++ src/main/java/com/adidas/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,18 @@
+package com.adidas.fizzbuzz;
+
+public class FizzBuzz {
+
+	/**
+	 * Return 'Fizz' if the number is divisible by 3
+	 * Return 'Buzz' if the number divisible by 5
+	 * Return 'FizzBuzz' if the number divisible by 3 & 5
+	 * Otherwise, return the given number
+	 * 
+	 * @param number	The next number in the sequence
+	 * 
+	 * @return String	T
+	 */
+	public static String getValue(int number) {
+		return null;
+	}
+}
